32c86587e71ba841987769a8778b005f75bdf831
LexieWhite <lexwhite028@gmail.com>
2020-03-01 17:58:33 -0600

    Initial commit 🎉

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
 
-export default App;
+  render() {
+    return(
+      <div className="content">
+        <h1>Lexie White</h1>
+        <p>I am learning React! I am currently a student at the University of Iowa</p>
+        <div className="list">
+        <ul>
+          <li>Disney World</li>
+          <li>Italy</li>
+          <li>Greece</li>
+        </ul>
+        </div>
+      </div>
+    )
+  }
+}
