933036bcb12d7daa3b28e41008f4af138a6fbf03
LexieWhite <lexwhite028@gmail.com>
2020-03-01 23:25:47 -0600

    Complete #3 #6

--- src/App.js
+++ src/App.js
@@ -7,19 +7,58 @@ import TodoItem from './components/TodoItem'
 // To do data
 import todosData from './todosData'
 
-function App() {
-  const todos = todosData.map(todo => <TodoItem key={todo.id} item={todo} />)
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: todosData
+    }
 
-  return(
-    <div className="content">
-      {todos}
-    </div>
-  )
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+  }
+
+  render() {
+    const todos = this.state.todos.map(todo => <TodoItem key={todo.id} item={todo}
+      handleChange={this.handleChange}/>)
+
+    return(
+      <div className="content">
+        {todos}
+      </div>
+    )
+  }
+
 }
 
-export default App
 
 // General notes:
 // - props is an object
 // - use curly braces around JS in JSX
 // - props can be "nested". a prop can be an object
+// - display logic goes after render but before return
+//    - display logic: what shows on the page
+// - state: data that the constructor maintains
+// - event handling: user interaction
+// - use setState any time you want to change the state
+//    - new version of state (need to use {} in the setState())
+//    - every time a function deals with state you have to bind it in the
+//    constructor
+//    - to get the last version of state use setState and a function with
+//    prevState
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,8 @@ import '../stylesheets/TodoItem.css'
 function TodoItem(props) {
   return(
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed}/>
+      <input type="checkbox" checked={props.item.completed}
+      onChange={() => props.handleChange(props.item.id)}/>
       <p>{props.item.text}</p>
     </div>
   )
