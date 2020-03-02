08598bcc86720759ae323c129209477d9fec7482
LexieWhite <lexwhite028@gmail.com>
2020-03-01 21:12:06 -0600

    Create todo component and style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import './App.css';
+import './stylesheets/App.css';
+
+// Components
+import TodoItem from './components/TodoItem'
 
 export default class App extends React.Component {
 
   render() {
     return(
       <div className="content">
-        <h1>Lexie White</h1>
-        <p>I am learning React! I am currently a student at the University of Iowa</p>
-        <div className="list">
-        <ul>
-          <li>Disney World</li>
-          <li>Italy</li>
-          <li>Greece</li>
-        </ul>
-        </div>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
       </div>
     )
   }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../stylesheets/TodoItem.css'
+
+function TodoItem() {
+  return(
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p>Placeholder text</p>
+    </div>
+  )
+}
+
+export default TodoItem
